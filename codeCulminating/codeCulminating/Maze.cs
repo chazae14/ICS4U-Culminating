973b2b7dd16a5fc1ee5104fc18f7e1ebc89a68b8
Jemuel Lateo <jlateo125@gmail.com>
2024-05-22 09:05:44 -0400

    Finished Maze

--- codeCulminating/codeCulminating/Maze.cs
+++ codeCulminating/codeCulminating/Maze.cs
@@ -12,16 +12,62 @@ namespace codeCulminating
 {
     public partial class Maze : Form
     {
+
+        Point startLocation; // Holds start location
+        int countDown = 0; // Used for game time
         public Maze()
         {
             InitializeComponent();
+            Start();
         }
 
-        private void Maze_Load(object sender, EventArgs e)
+        // Put cursor in start position and Start countdown
+        private void Start()
         {
-
+            GameTime.Start();
+            startLocation = lblStart.Location;
+            Cursor.Position = PointToScreen(startLocation);
+            countDown = 45;
+        }
+  
+        // When cursur hits a wall, reset its position to the start
+        private void MazeWall_MouseEnter(object sender, EventArgs e)
+        {
+            Cursor.Position = PointToScreen(startLocation);
         }
 
- 
+        // Game timer
+        private void GameTime_Tick(object sender, EventArgs e)
+        {
+            // Check if game time is 0
+            if (countDown < 0)
+            {
+                GameTime.Stop();
+                // Ask if player wants to continue
+                DialogResult choice = MessageBox.Show("You're out of time\nTry again?","Fail", MessageBoxButtons.YesNo);
+                if(choice == DialogResult.Yes)
+                {
+                    Start();
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+
+            lblTime.Text = countDown.ToString();
+            countDown--;
+        }
+
+        // End Game
+        private void lblEnd_MouseEnter(object sender, EventArgs e)
+        {
+            GameTime.Stop();
+            DialogResult choice = MessageBox.Show("Maze complete.", "Win`", MessageBoxButtons.OK);
+            if (choice == DialogResult.OK) 
+            {
+                this.Close();
+            }
+        }
     }
 }
