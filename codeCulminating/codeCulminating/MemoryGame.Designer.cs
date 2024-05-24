0e82f407c1828453c39a5a5ee8ed03191353426e
Jemuel Lateo <jlateo125@gmail.com>
2024-05-24 09:08:57 -0400

    more of memory game code

--- codeCulminating/codeCulminating/MemoryGame.Designer.cs
+++ codeCulminating/codeCulminating/MemoryGame.Designer.cs
@@ -37,103 +37,142 @@
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button1.Location = new System.Drawing.Point(12, 27);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(60, 60);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.myButtons_MouseDown);
+            this.button1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.myButtons_MouseUp);
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
             this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button2.Location = new System.Drawing.Point(78, 27);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(60, 60);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.myButtons_MouseDown);
+            this.button2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.myButtons_MouseUp);
             // 
             // button3
             // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
             this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button3.Location = new System.Drawing.Point(144, 27);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(60, 60);
             this.button3.TabIndex = 2;
             this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.myButtons_MouseDown);
+            this.button3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.myButtons_MouseUp);
             // 
             // button4
             // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
             this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button4.Location = new System.Drawing.Point(12, 93);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(60, 60);
             this.button4.TabIndex = 5;
             this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.myButtons_MouseDown);
+            this.button4.MouseUp += new System.Windows.Forms.MouseEventHandler(this.myButtons_MouseUp);
             // 
             // button5
             // 
+            this.button5.BackColor = System.Drawing.Color.Transparent;
             this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button5.Location = new System.Drawing.Point(78, 93);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(60, 60);
             this.button5.TabIndex = 4;
             this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.MouseDown += new System.Windows.Forms.MouseEventHandler(this.myButtons_MouseDown);
+            this.button5.MouseUp += new System.Windows.Forms.MouseEventHandler(this.myButtons_MouseUp);
             // 
             // button6
             // 
+            this.button6.BackColor = System.Drawing.Color.Transparent;
             this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button6.Location = new System.Drawing.Point(144, 93);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(60, 60);
             this.button6.TabIndex = 3;
             this.button6.Text = "button6";
-            this.button6.UseVisualStyleBackColor = true;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.MouseDown += new System.Windows.Forms.MouseEventHandler(this.myButtons_MouseDown);
+            this.button6.MouseUp += new System.Windows.Forms.MouseEventHandler(this.myButtons_MouseUp);
             // 
             // button7
             // 
+            this.button7.BackColor = System.Drawing.Color.Transparent;
             this.button7.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button7.Location = new System.Drawing.Point(12, 159);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(60, 60);
             this.button7.TabIndex = 8;
             this.button7.Text = "button7";
-            this.button7.UseVisualStyleBackColor = true;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.MouseDown += new System.Windows.Forms.MouseEventHandler(this.myButtons_MouseDown);
+            this.button7.MouseUp += new System.Windows.Forms.MouseEventHandler(this.myButtons_MouseUp);
             // 
             // button8
             // 
+            this.button8.BackColor = System.Drawing.Color.Transparent;
             this.button8.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button8.Location = new System.Drawing.Point(78, 159);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(60, 60);
             this.button8.TabIndex = 7;
             this.button8.Text = "button8";
-            this.button8.UseVisualStyleBackColor = true;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.MouseDown += new System.Windows.Forms.MouseEventHandler(this.myButtons_MouseDown);
+            this.button8.MouseUp += new System.Windows.Forms.MouseEventHandler(this.myButtons_MouseUp);
             // 
             // button9
             // 
+            this.button9.BackColor = System.Drawing.Color.Transparent;
             this.button9.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button9.Location = new System.Drawing.Point(144, 159);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(60, 60);
             this.button9.TabIndex = 6;
             this.button9.Text = "button9";
-            this.button9.UseVisualStyleBackColor = true;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.MouseDown += new System.Windows.Forms.MouseEventHandler(this.myButtons_MouseDown);
+            this.button9.MouseUp += new System.Windows.Forms.MouseEventHandler(this.myButtons_MouseUp);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(63, 237);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 9;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
             // MemoryGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(218, 250);
+            this.ClientSize = new System.Drawing.Size(218, 272);
+            this.Controls.Add(this.btnStart);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.button9);
@@ -161,5 +200,6 @@
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button btnStart;
     }
 }
--- codeCulminating/codeCulminating/MemoryGame.cs
+++ codeCulminating/codeCulminating/MemoryGame.cs
@@ -7,6 +7,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace codeCulminating
@@ -14,8 +15,9 @@ namespace codeCulminating
     public partial class MemoryGame : Form
     {
         Button[] myButtons;
+        List<int> myList;
         Random rnd = new Random();
-        int turn = 9;
+        int turn = 0;
         public MemoryGame()
         {
             InitializeComponent();
@@ -23,7 +25,13 @@ namespace codeCulminating
 
         private void MemoryGame_Load(object sender, EventArgs e)
         {
+            myList = new List<int>();
+            rnd = new Random();
+            myList.Add(rnd.Next(0,10));
+
+
             myButtons = new Button[] {button1,button2,button3,button4,button5,button6,button7,button8,button9};
+            ;
 
             for (int n = 0; n < myButtons.Count(); n++)
             {
@@ -31,11 +39,88 @@ namespace codeCulminating
                 myButtons[n].Click += new EventHandler(myButtons_Click);
             }
 
+
+            
+
+        }
+
+        private void Simon_Says()
+        {
+            Thread.Sleep(150);
+
+            foreach(int x in myList) 
+            {
+                switch (x)
+                {
+                    case 0:
+                        myButtons[x].BackColor = Color.Black;
+                        Thread.Sleep(1000);
+                        myButtons[x].BackColor = Color.Transparent;
+                        break;
+                    case 1:
+                        myButtons[x].BackColor = Color.Black;
+                        Thread.Sleep(1000);
+                        myButtons[x].BackColor = Color.Transparent;
+                        break;
+                    case 2:
+                        myButtons[x].BackColor = Color.Black;
+                        Thread.Sleep(1000);
+                        myButtons[x].BackColor = Color.Transparent;
+                        break; 
+                    case 3:
+                        myButtons[x].BackColor = Color.Black;
+                        Thread.Sleep(1000);
+                        myButtons[x].BackColor = Color.Transparent;
+                        break;
+                    case 4:
+                        myButtons[x].BackColor = Color.Black;
+                        Thread.Sleep(1000);
+                        myButtons[x].BackColor = Color.Transparent;
+                        break;
+                    case 5:
+                        myButtons[x].BackColor = Color.Black;
+                        Thread.Sleep(1000);
+                        myButtons[x].BackColor = Color.Transparent;
+                        break;
+                    case 6:
+                        myButtons[x].BackColor = Color.Black;
+                        Thread.Sleep(1000);
+                        myButtons[x].BackColor = Color.Transparent;
+                        break;
+                    case 7:
+                        myButtons[x].BackColor = Color.Black;
+                        Thread.Sleep(1000);
+                        myButtons[x].BackColor = Color.Transparent;
+                        break;
+                    case 8:
+                        myButtons[x].BackColor = Color.Black;
+                        Thread.Sleep(1000);
+                        myButtons[x].BackColor = Color.Transparent;
+                        break;
+                }
+            }
         }
 
         private void myButtons_Click(object sender, EventArgs e)
         {
-            
+            Button currentButton = (Button)sender;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            new Thread(Simon_Says).Start();
+        }
+
+        private void myButtons_MouseDown(object sender, MouseEventArgs e)
+        {
+            Button currentButton = (Button)sender;
+            currentButton.BackColor = Color.Black;          
+        }
+
+        private void myButtons_MouseUp(object sender, MouseEventArgs e)
+        {
+            Button currentButton = (Button)sender;
+            currentButton.BackColor = Color.Transparent;
         }
     }
 }
