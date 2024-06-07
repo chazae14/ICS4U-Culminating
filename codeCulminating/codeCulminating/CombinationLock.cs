95dd51174b364e89337df8b5b91a650079e2e369
Jemuel Lateo <jlateo125@gmail.com>
2024-06-07 08:54:52 -0400

    Combination lock

--- codeCulminating/codeCulminating/CombinationLock.cs
+++ codeCulminating/codeCulminating/CombinationLock.cs
@@ -17,26 +17,72 @@ namespace codeCulminating
             InitializeComponent();
         }
 
-        string userInput;
 
+        string userInput = " "; // Stores user input
+        string correctCode = "1234"; // Stores correct code - only make up to 4 digits
 
+        // Add Numbers to text box
         private void Buttons_MouseClick(object sender, MouseEventArgs e)
         {
             Button currentButton = (Button)sender;
 
-            userInput += currentButton.Text;
-
-            txtCombination.Text = userInput;
+            // Add number only up to 4 digits
+            if(userInput.Length < 4)
+            {
+                txtCombination.Text += currentButton.Text;
+                userInput = txtCombination.Text;
+            }          
         }
 
+        // Delete button event
         private void btnDelete_Click(object sender, EventArgs e)
         {
+            // Delete one digit in text box at a time and update user input
+            if ((String.Compare(txtCombination.Text, " ") < 0))
+            {
+                txtCombination.Text = txtCombination.Text.Substring(0, txtCombination.Text.Length - 1 + 1);
+                userInput = txtCombination.Text;
+            }
+            else
+            {
+                txtCombination.Text = txtCombination.Text.Substring(0, txtCombination.Text.Length - 1);
+                userInput = txtCombination.Text;
+            }
 
+            // if text box has nothing, then userInput is nothing
+            if (txtCombination.Text.Length == 0)
+            {
+                userInput = "";
+            }
         }
 
+        // Check if userInput is correct
         private void btnEnter_Click(object sender, EventArgs e)
         {
+            userInput = txtCombination.Text;
 
+            // If correct, exit window, else shake screen
+            if (userInput == correctCode)
+            {
+                this.Close();
+            }
+            else
+            {
+                Shake(this);
+            }
+        }
+
+        //Shake screen code
+        private static void Shake(Form form)
+        {
+            var original = form.Location;
+            var rnd = new Random(500);
+            const int shake_amplitude = 5;
+            for (int i = 0; i < shake_amplitude; i++)
+            {
+                form.Location = new Point(original.X + rnd.Next(-shake_amplitude, shake_amplitude), original.Y + rnd.Next(-shake_amplitude, shake_amplitude));
+                System.Threading.Thread.Sleep(20);
+            }
         }
 
     }
