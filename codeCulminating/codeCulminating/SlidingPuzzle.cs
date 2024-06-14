e1039dd06b4d0f26c68d472234444eea6c131383
Jemuel Lateo <jlateo125@gmail.com>
2024-06-14 09:12:27 -0400

    commifor comments

--- codeCulminating/codeCulminating/SlidingPuzzle.cs
+++ codeCulminating/codeCulminating/SlidingPuzzle.cs
@@ -55,16 +55,17 @@ namespace codeCulminating
             }
         }
 
-        // Get random sequence
+        // Get random sequence and check if puzzle is possible
         private int[] shuffle(int[] myButtons)
         {
             
-            int count = 0;
+            int count = 0; // keep track of inversion
             int inversion = 0;           
 
             Random rnd = new Random();
             myButtons = myButtons.OrderBy(x => rnd.Next()).ToArray();
 
+            // find inversion
             for (int i = 0; i < myButtons.Length - 1; i++)
             {
                 for (int j = i; j < myButtons.Length ; j++)
@@ -78,11 +79,13 @@ namespace codeCulminating
 
             inversion = count % 2;
 
+            // if # of inverion is even, then puzzle is possible
             if( inversion == 0)
             {
                 return myButtons;
             }
-  
+
+            // Call function again if # of inversions is odd 
             return shuffle(myButtons);
         }
 
